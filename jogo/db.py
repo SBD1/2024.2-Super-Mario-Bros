10741cfc105c87bc06f765115bc7895049af60ce
Alana Gabriele Amorim Silva <alanagabriele43@gmail.com>
2025-02-03 18:56:22 -0300

    feat: triggers

--- jogo/db.py
+++ jogo/db.py
@@ -1,15 +1,16 @@
 import psycopg2
 
+
 def connect_to_db():
     try:
         connection = psycopg2.connect(
             dbname="supermario",
-            user="root",
-            password="123456",
+            user="gamer",
+            password="senha_gamer",
             host="localhost",
             port="5432"
         )
-        print("Conectado ao banco de dados!")
+        # print("Conectado ao banco de dados!")
         return connection
     except Exception as e:
         print(f"Erro ao conectar ao banco de dados: {e}")
